d73038fd3c36674cecc6bec5fdab085641295810
Sangwon Kim <brave_bean@naver.com>
2026-07-15 15:24:54 +0900

    Win32 콘솔 코드페이지를 UTF-8로 설정해 한글 출력 깨짐 수정
    /utf-8로 컴파일된 한글 문자열 리터럴이 콘솔 기본 코드페이지(CP949 등)와
    맞지 않아 깨져 보이던 문제를 SetConsoleOutputCP/SetConsoleCP(CP_UTF8)로 해결.

--- SampleOrderSystem-ownovadoz-0715/main.cpp
+++ SampleOrderSystem-ownovadoz-0715/main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+namespace {
+
+void setupConsoleEncoding() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+}
+
+} // namespace
+
 #ifdef _DEBUG
 #include <gmock/gmock.h>
 
 int main(int argc, char** argv) {
+    setupConsoleEncoding();
     ::testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
 #else
 
 int main() {
+    setupConsoleEncoding();
     std::cout << "반도체 시료 생산주문관리 시스템\n";
     return 0;
 }
